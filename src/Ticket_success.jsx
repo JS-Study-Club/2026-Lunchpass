7ad998ffa836e08ab4a75001e064e7ed4dd576d6
oey135 <b01080126412@gmail.com>
2026-02-17 03:10:55 +0900

    예매 성공 페이지 디자인 반영

--- src/Ticket_success.jsx
+++ src/Ticket_success.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 import SuccessIcon from "./assets/success_icon.svg";
 import BackButton from "./assets/back_button.svg";
@@ -6,9 +7,11 @@ import BackButton from "./assets/back_button.svg";
 import "./Ticket_success.css";
 
 function Ticket_success() {
-    let [day, setDay] = useState("월");
-    let [date, setDate] = useState("오늘({day})");
+    const [date, setDate] = useState('날짜');
+    const [time, setTime] = useState('시간대');   
 
+    useEffect(() => {setDate(todayDOW())},[]);
+    
 
     return (
         <div className='Main'>
@@ -16,14 +19,24 @@ function Ticket_success() {
             <div id='success'>
                 <img src={SuccessIcon} alt="성공아이콘" id='successIcon'/>
                 <span id='successMesssage'>예매 성공!</span>
-                <span id='subMessage'>오늘 석식 티켓을 예매하였습니다</span>
+                <span id='subMessage'>티켓을 성공적으로 예매하였습니다</span>
             </div>
             <div id='info'>
-                <span></span>
+                <div id='infoText'>
+                    <div className='dateNtime'>
+                        <span>사용가능 날짜</span>
+                        <span className='info_em'>{date}</span>
+                    </div>
+                    <div className='dateNtime'>
+                        <span>조식/석식</span>
+                        <span className='info_em'>{time}</span>
+                    </div>
+                    
+                </div>
             </div>
             <div id='closeSet'>
                 <Link to="/fail" id='closeBtn'><span>닫기</span></Link>
-                <Link to="/fail" id='closeText'><span>내 티켓으로 이동</span></Link>
+                <Link to="/myticket" id='closeText'><span>내 티켓으로 이동</span></Link>
             </div>
         </div>
   );
@@ -34,10 +47,22 @@ function BackBtn() {
         <img src={BackButton} alt="뒤로가기버튼" id='topBack'
         style={{
         position: 'absolute',
-        top: '55px',
+        top: '9%',
         left: '20px'}}/>
     );
 }
 
+function todayDOW() {
+    const day = new Date();
+    let DOW = '';
+    switch(day.getDay()) {
+        case 1: DOW = '월'; break;
+        case 2: DOW = '화'; break;
+        case 3: DOW = '수'; break;
+        case 4: DOW = '목'; break;
+        case 5: DOW = '금'; break;
+    }
+    return `오늘(${DOW})`;
+}
 
 export default Ticket_success;
