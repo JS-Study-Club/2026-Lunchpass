501e48f9d286f91c72a2f18cc63030fff9d072d7
leeyeji0306 <s2548@e-mirim.hs.kr>
2026-02-23 04:13:42 +0900

    보유한/취소한 티켓 페이지와 티켓 취소, 회수 요청 모달 연동

--- src/Components/HaveCancelTicket.jsx
+++ src/Components/HaveCancelTicket.jsx
@@ -1,29 +1,51 @@
 import styled from "styled-components";
-// import { useState } from 'react';
+import { useState } from 'react';
 import NoneTicket from "/assets/img/NoneTicket.svg"
 
+import View_qr from "../Components/View_qr_M.jsx";
+import TicketCancle from "../Components/TicketCancle_M.jsx";
+import Retrieval from "./Retrieval_M.jsx";
+
 export default function HaveCancelTicket({Selection}){
+    const [isOpen, setIsOpen] = useState(false);
     return(
-        <TicketBoxContainer>
-            <TicketBox>
-                <Ticket>{"12 / 01  (월)"}&nbsp;&nbsp;{"조식"}</Ticket>
-                <QR>{Selection === "Have" ? "QR 보기" : "회수 요청"}</QR>
-            </TicketBox>
-            <TicketBox>
-                <Ticket>{"12 / 01  (월)"}&nbsp;&nbsp;{"석식"}</Ticket>
-                <QR>{Selection === "Have" ? "QR 보기" : "회수 요청"}</QR>
-            </TicketBox>
-            <TicketBox>
-                <Ticket>{"12 / 02  (화)"}&nbsp;&nbsp;{"조식"}</Ticket>
-                <QR>{Selection === "Have" ? "QR 보기" : "회수 요청"}</QR>
-            </TicketBox>
-            <TicketBox>
-                <Ticket>{"12 / 02  (화)"}&nbsp;&nbsp;{"석식"}</Ticket>
-                <QR>{Selection === "Have" ? "QR 보기" : "회수 요청"}</QR>
-            </TicketBox>
-            {/* <NoneTicketContainer src={NoneTicket} /> */}
-            {/* 티켓 없을 때 */}
-        </TicketBoxContainer>
+        <>
+            <TicketBoxContainer>
+                <TicketBox>
+                    <Ticket>{"12 / 01  (월)"}&nbsp;&nbsp;{"조식"}</Ticket>
+                    <QR onClick={() => setIsOpen(Selection === "Have" ? "VIEW_QR" : "RETRIEVAL")}>{Selection === "Have" ? "QR 보기" : "회수 요청"}</QR>
+                </TicketBox>
+                <TicketBox>
+                    <Ticket>{"12 / 01  (월)"}&nbsp;&nbsp;{"석식"}</Ticket>
+                    <QR onClick={() => setIsOpen(Selection === "Have" ? "VIEW_QR" : "RETRIEVAL")}>{Selection === "Have" ? "QR 보기" : "회수 요청"}</QR>
+                </TicketBox>
+                <TicketBox>
+                    <Ticket>{"12 / 02  (화)"}&nbsp;&nbsp;{"조식"}</Ticket>
+                    <QR onClick={() => setIsOpen(Selection === "Have" ? "VIEW_QR" : "RETRIEVAL")}>{Selection === "Have" ? "QR 보기" : "회수 요청"}</QR>
+                </TicketBox>
+                <TicketBox>
+                    <Ticket>{"12 / 02  (화)"}&nbsp;&nbsp;{"석식"}</Ticket>
+                    <QR onClick={() => setIsOpen(Selection === "Have" ? "VIEW_QR" : "RETRIEVAL")}>{Selection === "Have" ? "QR 보기" : "회수 요청"}</QR>
+                </TicketBox>
+                {/* <NoneTicketContainer src={NoneTicket} /> */}
+                {/* 티켓 없을 때 */}
+            </TicketBoxContainer>
+            {
+                isOpen === "CANCLE" && (
+                    <TicketCancle setIsOpen={setIsOpen} />
+                )
+            }
+            {
+                isOpen === "VIEW_QR" && (
+                    <View_qr setIsOpen={setIsOpen}/>
+                )
+            }
+            {
+                isOpen === "RETRIEVAL" && (
+                    <Retrieval setIsOpen={setIsOpen}/>
+                )
+            }
+            </>
     )
 }
 
